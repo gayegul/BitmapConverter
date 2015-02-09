59f8fb862224facb07e02b6b7605d7d133e5a8dd
Gaye Bulut <gayegul@gmail.com>
2015-02-09 00:12:47 -0800

    added index, converter, grunt and package.json

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
-// Bitmap Transformer
-// Code Fellows JavaScript Development Accelerator
-// Due 2-9-15
-// ---------------------------
-// Gaye Bulut
-// Tricia Buckenberger
-//  Sam Hamm
-// ---------------------------
-
 'use strict';
 
 var fs = require('fs');
-var bitmap = fs.readFileSync('test2.bmp');
+var bitmap = fs.readFileSync('test.bmp');
+var BitmapChanger = require('./lib/bmpChanger.js');
 
-var bitmapObject = {};
-
-bitmapObject.type = bitmap.toString('utf-8', 0, 2); // reads type
-bitmapObject.size = bitmap.readUInt32LE(2); // reads size
-bitmapObject.startOfPixels = bitmap.readUInt32LE(10);
-bitmapObject.width = bitmap.readUInt32LE(18);
-bitmapObject.height = bitmap.readUInt32LE(22);
-bitmapObject.paletteSize = bitmap.readUInt32LE(46);
-bitmapObject.colorDepth = bitmap.readUInt16LE(28);
-var headerSize = 54;
-
-console.dir(bitmapObject);
-
-function BitmapChange(image) {
-	this.invoke = function() {
-		for (var i = headerSize; i < headerSize + bitmapObject.paletteSize; i++){
-			image[i] = 255 - image[i];
-		};
-	};
-	this.toBuffer = function () {return image;}
-}
 
 var buf = new Buffer(bitmap);
-var changer = new BitmapChange(buf);
-changer.invoke();
+var changer = new BitmapChanger(buf);
+var buf2 = new Buffer(bitmap);
+var changer2 = new BitmapChanger(buf2);
+var buf3 = new Buffer(bitmap);
+var changer3 = new BitmapChanger(buf3);
 
-fs.writeFile('output2.bmp', changer.toBuffer() );
+changer.removeRed();
+fs.writeFile('noRed.bmp', changer.toBuffer() );
+
+changer2.removeGreen();
+fs.writeFile('noGreen.bmp', changer2.toBuffer() );
+
+changer3.invert();
+fs.writeFile('inverted.bmp', changer3.toBuffer() );
 
